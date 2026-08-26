3dab2f2aed8f354c940b76b19b150c66e66e1e6b
KingFraxions <markowazerman182@gmail.com>
2026-08-26 15:45:04 +0200

    Update base.lua

--- src/games/bedwars/6872274481 - game/base.lua
+++ src/games/bedwars/6872274481 - game/base.lua
@@ -31,10 +31,6 @@ local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
 local assetfunction = getcustomasset
 
-local kickThread = task.spawn(lplr.Kick, lplr, 'Bedwars is no longer supported by Vape V4, thank you for 5 years of support ❤️')
-if coroutine.status(kickThread) ~= 'dead' then
-	game:Shutdown()
-end
 
 local vape = shared.vape
 local entitylib = vape.Libraries.entity
